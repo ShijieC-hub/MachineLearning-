98d5818009fd6f96a3f94a981b8384c3645afc69
zyf <3092638690@qq.com>
2021-12-30 23:32:50 +0800

    版本提交-1

--- java/Algo.java
+++ java/Algo.java
@@ -1,16 +1,69 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Algo {
     /**
-     *
+     * image和 sampleImg长宽应该一致
      * @param image 传入需要比对的图片
      * @param sampleImg 样本图片
-     * @param value 样本图片对应的值
      * @return 相似度
      */
-    public int compareImage(int[][] image,int[][] sampleImg,int value){
+    public int compareImage(BufferedImage image,BufferedImage sampleImg){
+
+        //先都二值化
+        BufferedImage image2 = erzhi(image,128);
+        BufferedImage sampleImg2 = erzhi(sampleImg,128);
+
+        int wight = 28;
+        int height = 28;
+        int num = 0;//相似度
+
+        //如果像素点一样，num++
+        for (int i = 0; i < wight; i++) {
+            for (int j = 0; j < height; j++) {
+                if(image2.getRGB(i,j) == Color.black.getRGB() && image2.getRGB(i,j) == sampleImg2.getRGB(i,j)){
+                    num++;
+                }
+            }
+        }
 
 
-        return 0;
+        return num;
     }
+
+    //获取灰度
+    public int getgray(BufferedImage bufferedImage,int x,int y){
+
+        int color = bufferedImage.getRGB(x,y);
+        int r = (color >> 16) & 0xff;
+        int g = (color >> 8 ) & 0xff;
+        int b = color & 0xff;
+        int gray = (int)(0.2989*r + 0.5870*g + 0.1140*b);
+
+        return gray;
+
+    }
+
+    //二值化
+    public BufferedImage erzhi(BufferedImage bufferedImage,int k){
+
+        int width = bufferedImage.getWidth();
+        int hight = bufferedImage.getHeight();
+
+        BufferedImage bf = new BufferedImage(width,hight,BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < hight; j++) {
+                if(getgray(bufferedImage,i,j) > k){
+                    bf.setRGB(i,j, Color.WHITE.getRGB());
+                }else {
+                    bf.setRGB(i,j,Color.BLACK.getRGB());
+                }
+            }
+        }
+
+        return bf;
+    }
+
+
+
 }
--- java/MnistRead.java
+++ java/MnistRead.java
@@ -1,15 +1,19 @@
-import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class MnistRead {
+public class MnistRead extends JPanel{
     public static final String TRAIN_IMAGES_FILE = "resources/train-images.idx3-ubyte";
     public static final String TRAIN_LABELS_FILE = "resources/train-labels.idx1-ubyte";
-    public static final String TEST_IMAGES_FILE = "resources/t10k-images.idx3-ubyte";
-    public static final String TEST_LABELS_FILE = "resources/t10k-labels.idx1-ubyte";
+//    public static final String TEST_IMAGES_FILE = "resources/t10k-images.idx3-ubyte";
+//    public static final String TEST_LABELS_FILE = "resources/t10k-labels.idx1-ubyte";
 
 
 
@@ -20,7 +24,7 @@ public class MnistRead {
      * @return one row show a `picture`
      */
     public static double[][] getImages(String fileName) {
-        double[][] x = null;
+        double[][] x;
         try (BufferedInputStream bin = new BufferedInputStream(new FileInputStream(fileName))) {
             byte[] bytes = new byte[4];
             bin.read(bytes, 0, 4);
@@ -72,10 +76,9 @@ public class MnistRead {
      * get labels of `train` or `test`
      *
      * @param fileName the file of 'train' or 'test' about label
-     * @return
      */
     public static double[] getLabels(String fileName) {
-        double[] y = null;
+        double[] y;
         try (BufferedInputStream bin = new BufferedInputStream(new FileInputStream(fileName))) {
             byte[] bytes = new byte[4];
             bin.read(bytes, 0, 4);
@@ -101,9 +104,8 @@ public class MnistRead {
      * @param pixelValues pixelValues and ordered by column.
      * @param width       width
      * @param high        high
-     * @param fileName    image saved file.
      */
-    public static void drawGrayPicture(double[] pixelValues, int width, int high, String fileName) throws IOException {
+    public static BufferedImage drawGrayPicture(double[] pixelValues, int width, int high){
         BufferedImage bufferedImage = new BufferedImage(width, high, BufferedImage.TYPE_INT_RGB);
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < high; j++) {
@@ -112,27 +114,120 @@ public class MnistRead {
                 bufferedImage.setRGB(j, i, value);
             }
         }
-        ImageIO.write(bufferedImage, "JPEG", new File(fileName));
+        return bufferedImage;
+//        ImageIO.write(bufferedImage, "JPEG", new File(fileName));
     }
 
-    public static void main(String[] args) {
-        double[][] images = getImages(TRAIN_IMAGES_FILE);
-        double[] labels = getLabels(TRAIN_LABELS_FILE);
-        int name = 0;
+    public static void main(String[] args) throws IOException {
+        MnistRead mnistRead = new MnistRead();
+        mnistRead.initUI();
 
-        for (int i = 0; i < 10; i++) {
-            try {
-                drawGrayPicture(images[i], 28, 28, "Test" + name ++ + ".jpg");
-            } catch (IOException exception) {
-                exception.printStackTrace();
+
+    }
+
+    public void initUI(){
+        JFrame jf = new JFrame("数字识别");
+        jf.setSize(1000,1000);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        BorderLayout borderLayout = new BorderLayout();
+        jf.setLayout(borderLayout);
+
+        jf.add(this,BorderLayout.CENTER);
+
+        JPanel east = new JPanel();
+        east.setPreferredSize(new Dimension(200,0));
+        BufferedImage bufferedImage = new BufferedImage(800,1000,3);
+        east.setBackground(Color.cyan);
+        jf.add(east,BorderLayout.EAST);
+
+        jf.setVisible(true);
+
+        Graphics g = this.getGraphics();
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setStroke(new BasicStroke(150));
+        Graphics imgg = bufferedImage.getGraphics();
+        Graphics2D img2d = (Graphics2D)imgg;
+        img2d.setStroke(new BasicStroke(150));
+
+        JButton jButton = new JButton("识别");
+        jButton.addActionListener(new ActionListener() {
+            //点击按钮，识别图片
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BufferedImage bf= changeSize(bufferedImage,28,28);
+                for (int i = 0; i < bf.getWidth(); i++) {
+                    for (int j = 0; j < bf.getHeight(); j++) {
+                        System.out.print(bf.getRGB(i, j));
+                    }
+                    System.out.println();
+                }
+//                g.drawImage(bf,100,100,null);
+//                g.drawLine(0,0,100,100);
+//                System.out.println("a");
+                distinguish(bf);
+                for (int i = 0; i < bf.getWidth(); i++) {
+                    for (int j = 0; j < bf.getHeight(); j++) {
+                        System.out.print(bf.getRGB(i, j));
+                    }
+                    System.out.println();
+                }
+            }
+        });
+        east.add(jButton);
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            int x1,y1,x2,y2;
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if(x1==0 && y1==0){
+                    x1 = e.getX();
+                    y1 = e.getY();
+                }else {
+                    x2 = e.getX();
+                    y2 = e.getY();
+                    g2d.drawLine(x1,y1,x2,y2);
+                    imgg.drawLine(x1,y1,x2,y2);
+                    x1 = x2;
+                    y1 = y2;
+                }
             }
 
-            System.out.println(labels[i]);
-        }
+            @Override
+            public void mouseMoved(MouseEvent e) {
 
+            }
+        });
 
     }
 
+    //识别图片
+    public void distinguish(BufferedImage image){
+        double[][] images = getImages(TRAIN_IMAGES_FILE);
+        double[] labels = getLabels(TRAIN_LABELS_FILE);
+        int similarNum = 0;//最高相似度
+        int value = -1;//最高相似度对应的值
+        int similar = 0;//当前相似度
+        Algo algo = new Algo();
+        for (int i = 0; i < images.length; i++) {
+            BufferedImage sampleImg = drawGrayPicture(images[i],28,28);//获取i位置的样本图片
+            similar = algo.compareImage(image,sampleImg);//对比两个图片,返回相似度
+            if(similar > similarNum){
+                similarNum = similar;
+                value = (int)labels[i];
+            }
+//            System.out.println(labels[i]);
+        }
+        System.out.println(value);
+    }
 
+    //改变图片大小
+    public BufferedImage changeSize(BufferedImage bf,int w,int h){
+        BufferedImage bufferedImage = new BufferedImage(w,h,3);
+        Graphics g = bufferedImage.getGraphics();
+        g.drawImage(bf,0,0,w,h,null);
+        return bufferedImage;
+    }
 
 }
